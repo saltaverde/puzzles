f3fb0545a5f02e8478505f7347dcec76ebee60e3
Ben Coleman <jbfcoleman@gmail.com>
2022-02-15 09:31:07 -0800

    Implement multiprocessing solution

--- spelling-bee.py
+++ spelling-bee.py
@@ -1,29 +1,60 @@
 import json
 import argparse
+import multiprocessing
+
+
+MINIMUM_WORD_LENGTH = 4
+DISTINCT_LETTER_COUNT = 7
+DICTIONARY_FILE_PATH = '/usr/share/dict/american-english'
+
+
+def partition(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i+n]
+
+
+def assign_to_map(data):
+    base_word_map, candidates = data
+    sub_map = {}
+
+    for candidate in candidates:
+        key = set(candidate)
+        for k in base_word_map:
+            if key.issubset(k):
+                sub_map.setdefault(k, set()).add(candidate)
+
+    return sub_map
 
 
 def populate_word_map():
     word_map = {}
 
-    with open('/usr/share/dict/american-english', 'r') as f:
-        candidates = {word for word in f.read().split() if word[0].islower() and len(word) > 3}
+    with open(DICTIONARY_FILE_PATH) as f:
+        candidates = {
+            word for word in f.read().split()
+            # filter out proper nouns
+            if word[0].islower()
+            and len(word) >= MINIMUM_WORD_LENGTH
+            # no possessives
+            and not word.endswith("'s")
+        }
 
     for candidate in candidates:
-        if candidate.endswith("'s"):
-            pass
-        else:
-            key = ''.join(sorted({ l for l in candidate }))
-            if len(key) == 7:
-                word_map.setdefault(key, set()).add(candidate)
+        # sort the distinct letters in each word
+        key = ''.join(sorted({ l for l in candidate }))
+        if len(key) == DISTINCT_LETTER_COUNT:
+            word_map.setdefault(key, set()).add(candidate)
 
-    for candidate in candidates:
-        if candidate.endswith("'s"):
-            pass
-        else:
-            key = set(candidate)
-            for k in word_map:
-                if key.issubset(k):
-                    word_map[k].add(candidate)
+    with multiprocessing.Pool() as pool:
+        results = pool.map(assign_to_map,
+            (
+                (word_map, cands) for cands in partition(list(candidates), int(len(candidates)/multiprocessing.cpu_count()) + 1)
+            )
+        )
+
+    for result in results:
+        for key, values in result.items():
+            word_map[key] |= values
 
     with open('spelling-bee-map.json', 'w') as f:
         json.dump({ k: list(v) for k, v in word_map.items() }, f)
